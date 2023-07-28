50693622bd42e2547f48c8cd69bd91d4e0a77428
Emre Can UĞURLU <emrecnugurlu@gmail.com>
2023-07-29 00:35:55 +0300

    ref keywordü nedir? Nasıl kullanılır?

---
+++ refKeyword/refKeyword/Program.cs
@@ -0,0 +1,17 @@
+﻿#region ref keywordü nedir? Ne amaçla kullanılır?
+//ref keywordü referanstan gelmektedir.
+//OOP'de referanslar "=" operatörü ile iletişime geçmektedirler. Bir referans işaretlendiği herhangi bir nesneyi "=" operatörü sayesinde farklı bir referansa işaretleyebilir.
+//Değer türlü değişkenlerde referans çalışması yapmak istiyorsak eğer ref keywordü kullanılır.
+//ref keywordü değer türlü değişkenlerin referans türlü değişkenler gibi çalışmasını sağlayan bir komuttur.
+
+
+int b = 6;
+
+ref int a = ref b;
+
+b -= 10;
+
+Console.WriteLine(a);
+
+
+#endregion
